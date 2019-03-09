3e2947cbb8ba6c72923fbd76d24a6d3e684735b4
Savannah C <sgcorrell@gmail.com>
2019-03-09 21:17:41 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,17 +18,17 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        :name => "Lord Capulet"
-        :age => "50"
+        name: "Lord Capulet"
+        age: "50"
         },
       :matriarch => {
-        :name => "Lady Capulet"
-        :age => "51"
+        name: "Lady Capulet"
+        age: "51"
         },
       :heroine => {
-        :name => "Juliet"
-        :age => "15"
-        :status => "alive"
+        name: "Juliet"
+        age: "15"
+        status: "alive"
         },
       :heroine_friends => []
    }
